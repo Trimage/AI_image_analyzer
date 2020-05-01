75226fa8012f1752505b4c461fcf8f614ad50fb3
Trimage <seokwon0127@naver.com>
2020-05-01 20:05:43 +0900

    '탐색' 버튼 클릭시마다 데이터조회하도록 수정

--- face_ai_api.py
+++ face_ai_api.py
@@ -9,11 +9,13 @@ face_url = "https://openapi.naver.com/v1/vision/face" # 얼굴감지
 files = {'image': open('슴성_거니.png', 'rb')}
 headers = {'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
 
-face_response = requests.post(face_url,  files=files, headers=headers)
-face_rescode = face_response.status_code
-
-
-if(face_rescode==200):
-    face_data = json.loads(face_response.text)
-else:
-    print("Error Code:" + face_rescode)
+def request_data() :
+    face_response = requests.post(face_url,  files=files, headers=headers)
+    face_rescode = face_response.status_code
+    
+    if(face_rescode==200) :
+        face_data = json.loads(face_response.text)
+        return face_data
+    else:
+        print("Error Code:" + face_rescode)
+        return celebrity_rescode
